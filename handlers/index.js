d875715b58375e59280e1f03649bd39a92347bdb
dmi3y <lapshukov@gmail.com>
2016-06-05 23:41:55 -0700

    fixin up hardcoded origin, more debug messages

--- handlers/index.js
+++ handlers/index.js
@@ -1,12 +1,15 @@
 var router = require('express').Router()
-var origin = 'http://localhost:3000/'
+var origin = 'http://localhost:8080/'
+var debug = require('debug')('auth:handlers:index')
 
 router.get('/ok/:token', (req, res) => {
   var token = req.params.token
+  debug(`success ${token}`)
   res.redirect(`${origin}?token=${token}`)
 })
 
 router.get('/no', (req, res) => {
+  debug('failture')
   res.redirect(`${origin}?token=null`)
 })
 
