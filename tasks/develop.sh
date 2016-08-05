c764141529ff9d6b70f4493ba6974b1597e3c733
dmi3y <dmitryl@syapse.com>
2016-08-04 21:17:12 -0700

    this is better

--- tasks/develop.sh
+++ tasks/develop.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 APP_ENV_VARS=$(cat .env)
 echo $APP_ENV_VARS
-env $APP_ENV_VARS supervisor "$@" server.js
+env $APP_ENV_VARS node "$@" server.js
 SERVER_PID=$!
 
 trap "kill $SERVER_PID" SIGINT SIGTERM
