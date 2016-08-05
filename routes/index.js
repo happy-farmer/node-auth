56cceb096d59186b0d06f92056e0894c13568514
dmi3y <dmitryl@syapse.com>
2016-08-04 22:12:55 -0700

    redirects > redirect

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var router = require('express').Router()
 
 router.use(require('./user'))
 router.use(require('./private'))
-router.use(require('./redirects'))
+router.use(require('./redirect'))
 
 module.exports = router
