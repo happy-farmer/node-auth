9edb4c3515bb5c122e749c931609850fa9fe9d72
dmi3y <dmitryl@syapse.com>
2016-08-04 21:27:55 -0700

    supervisor super charged power

--- tasks/develop.sh
+++ tasks/develop.sh
@@ -2,7 +2,7 @@
 
 APP_ENV_VARS=$(cat .env)
 echo $APP_ENV_VARS
-env $APP_ENV_VARS node "$@" server.js
+env $APP_ENV_VARS supervisor "$@" server.js
 SERVER_PID=$!
 
 trap "kill $SERVER_PID" SIGINT SIGTERM
